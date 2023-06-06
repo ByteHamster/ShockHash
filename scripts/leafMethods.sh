428760a4410b373a7090d6e54a7599e627958997
ByteHamster <info@bytehamster.com>
2023-06-06 17:17:42 +0200

    Add speedup script between ShockHash and RecSplit

---
+++ scripts/leafMethods.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+hostname
+strings recsplit_construction | grep " -m"
+strings Benchmark | grep " -m"
+
+# shellcheck disable=SC2086
+for leafSize in $(seq 2 1 18); do
+    for bucketSize in 100 500 1000 2000; do
+        params="--numObjects 300k --numQueries 0 --leafSize $leafSize --bucketSize $bucketSize"
+        ./recsplit_construction $params --leafMethod bruteforce
+        ./recsplit_construction $params --leafMethod rotations
+    done
+done
+
+# shellcheck disable=SC2086
+for leafSize in $(seq 2 2 40); do
+    for bucketSize in 100 500 1000 2000; do
+        params="--numObjects 300k --numQueries 0 --leafSize $leafSize --bucketSize $bucketSize"
+        ./Benchmark $params
+    done
+done
