4adf59cf8db10f6259a907cf36bbf589ef98171b
ByteHamster <info@bytehamster.com>
2023-07-20 13:48:25 +0200

    Update benchmark script

--- scripts/leafMethods.sh
+++ scripts/leafMethods.sh
@@ -17,5 +17,14 @@ for leafSize in $(seq 2 2 40); do
     for bucketSize in 100 500 1000 2000; do
         params="--numObjects 300k --numQueries 0 --leafSize $leafSize --bucketSize $bucketSize"
         ./Benchmark $params
+        ./Benchmark $params --rotate
+    done
+done
+
+# shellcheck disable=SC2086
+for leafSize in $(seq 42 2 50); do
+    for bucketSize in 100 500 1000 2000; do
+        params="--numObjects 300k --numQueries 0 --leafSize $leafSize --bucketSize $bucketSize"
+        ./Benchmark $params --rotate
     done
 done
