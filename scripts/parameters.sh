e8aa658d2378dc3011a07a38f741f10b11a224ca
ByteHamster <info@bytehamster.com>
2023-08-02 14:29:08 +0200

    Adapt benchmark to paper selection

--- scripts/parameters.sh
+++ scripts/parameters.sh
@@ -5,11 +5,8 @@ strings BenchmarkSIMD | grep " -m"
 
 # shellcheck disable=SC2086
 for leafSize in $(seq 4 2 40); do
-    for bucketSize in 5 50 500 2000; do
-        params="--numObjects 500k --numQueries 0 --leafSize $leafSize --bucketSize $bucketSize"
-        ./Benchmark $params
-        ./Benchmark $params --rotate
-        ./BenchmarkSIMD $params
-        ./BenchmarkSIMD $params --rotate
-    done
+    params="--numObjects 500k --numQueries 0 --leafSize $leafSize --bucketSize 2000"
+    ./Benchmark $params
+    ./BenchmarkSIMD $params
+    ./BenchmarkSIMD $params --rotate
 done
