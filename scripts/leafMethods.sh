883a3a74ef82b6258d0097c20ba0b30b0f9c7764
ByteHamster <info@bytehamster.com>
2023-08-02 15:39:32 +0200

    Larger N in benchmarks

--- scripts/leafMethods.sh
+++ scripts/leafMethods.sh
@@ -6,7 +6,7 @@ strings Benchmark | grep " -m"
 # shellcheck disable=SC2086
 for leafSize in $(seq 2 1 18); do
     for bucketSize in 100 500 1000 2000; do
-        params="--numObjects 300k --numQueries 0 --leafSize $leafSize --bucketSize $bucketSize"
+        params="--numObjects 1M --numQueries 0 --leafSize $leafSize --bucketSize $bucketSize"
         ./recsplit_construction $params --leafMethod bruteforce
         ./recsplit_construction $params --leafMethod rotations
     done
@@ -15,7 +15,7 @@ done
 # shellcheck disable=SC2086
 for leafSize in $(seq 2 2 40); do
     for bucketSize in 100 500 1000 2000; do
-        params="--numObjects 300k --numQueries 0 --leafSize $leafSize --bucketSize $bucketSize"
+        params="--numObjects 1M --numQueries 0 --leafSize $leafSize --bucketSize $bucketSize"
         ./Benchmark $params
         ./Benchmark $params --rotate
     done
@@ -24,7 +24,7 @@ done
 # shellcheck disable=SC2086
 for leafSize in $(seq 42 2 50); do
     for bucketSize in 100 500 1000 2000; do
-        params="--numObjects 300k --numQueries 0 --leafSize $leafSize --bucketSize $bucketSize"
+        params="--numObjects 1M --numQueries 0 --leafSize $leafSize --bucketSize $bucketSize"
         ./Benchmark $params --rotate
     done
 done
--- scripts/parameters.sh
+++ scripts/parameters.sh
@@ -5,7 +5,7 @@ strings BenchmarkSIMD | grep " -m"
 
 # shellcheck disable=SC2086
 for leafSize in $(seq 4 2 40); do
-    params="--numObjects 500k --numQueries 0 --leafSize $leafSize --bucketSize 2000"
+    params="--numObjects 1M --numQueries 0 --leafSize $leafSize --bucketSize 2000"
     ./Benchmark $params
     ./BenchmarkSIMD $params
     ./BenchmarkSIMD $params --rotate
