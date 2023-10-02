1de3efb5e46ec6c5fc5077cc0a9a10044e497331
ByteHamster <info@bytehamster.com>
2023-10-02 13:29:22 +0200

    Fix RiceBitVector for log2golomb > 57

--- include/RiceBitVector.h
+++ include/RiceBitVector.h
@@ -178,8 +178,10 @@ template <sux::util::AllocType AT = sux::util::AllocType::MALLOC> class RiceBitV
                     result += pos;
                     result <<= log2golomb;
 
-                    uint64_t fixed;
-                    memcpy(&fixed, (uint8_t *)&data + curr_fixed_offset / 8, 8);
+                    // In the worst case, curr_fixed_offset is 7.
+                    // Loading only 64 bit here would then fail for log2golomb>(64-7)=57
+                    __uint128_t fixed;
+                    memcpy(&fixed, (uint8_t *)&data + curr_fixed_offset / 8, 16);
                     result |= (fixed >> curr_fixed_offset % 8) & ((uint64_t(1) << log2golomb) - 1);
                     curr_fixed_offset += log2golomb;
                     return result;
